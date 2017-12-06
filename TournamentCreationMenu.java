a9ab5f07819eca1aa0e82b9d888862fc32dce75e
RileyBowling <rdbowling@cps.edu>
2017-12-06 16:09:20 -0600

    UltimateMenu Refinements

--- TournamentCreationMenu.java
+++ TournamentCreationMenu.java
@@ -1,10 +1,19 @@
 import java.awt.GridLayout;
 
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
 public class TournamentCreationMenu extends UltimateMenu {
 	public TournamentCreationMenu() {
 		makeWindow();
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
 		
-		GridLayout g = new GridLayout(4,1);
+		GridLayout g = new GridLayout(2,1);
         getWindow().setLayout(g);
+        
+        JLabel lbl = new JLabel("Enter Tournament Name");
+        JTextField txt = new JTextField();
+        getWindow().add(lbl);
+        getWindow().add(txt);
 	}
 }
